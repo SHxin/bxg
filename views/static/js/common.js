06ad54631ed92ace9dee62448d112e84d90a2dfd
SHxin <984502528@qq.com>
2017-10-10 23:20:28 +0800

    登录功能完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,17 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$(function () {
+	
+  //侧边栏用户信息的动态切换-------------------------------
+  //不是login页面,要执行的代码
+  if(location.pathname != '/dashboard/login'){
+    
+    //1-从cookie中获取userinfo的信息
+    var userinfo = $.cookie('userinfo');
+    userinfo = JSON.parse(userinfo);
+    
+    //2-使用模板引擎将获取到的信息展示到侧边栏
+    var str = template('profile-tpl',userinfo);
+    $('#user-info').html(str);
+    
+  }
+  
+});
