51ab5b8b6eeba094433c6bec40c4b5193591cc84
SHxin <984502528@qq.com>
2017-10-10 20:18:35 +0800

    优化结构代码

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
 
-    //1. 要知道用户请求网页的时候连接的格式
-    // http://studyit.com/index.php/文件夹名称/文件名称
-
-
-    //2. 需要通过php代码获取到用户要请求的文件夹名称以及文件名
-
-    //如果用户没有传递文件夹以及文件名的信息，我们应该让他默认显示首页
-
+    //1-默认值,首页
     $path = "/dashboard/index";
 
-    // var_dump($_SERVER);
+    //2-如果存在,跳转到对应的页面
     if(array_key_exists('PATH_INFO', $_SERVER)){
         $path = $_SERVER['PATH_INFO'];
     }
 
-    //3. 获取到文件夹名称以及文件名之后，要将文件拿到，返回给浏览器
-
+    //3-拿到文件,返回给浏览器,如果拿不到,返回404
     if(file_exists("views/".$path.".html")){
-        include "views/".$path.".html";
+        include $_SERVER["DOCUMENT_ROOT"]."/views/".$path.".html";
     }else{
         header("HTTP/1.1 404 NotFound");
-        echo "404";
+        echo "404 NotFound";
     }
 
-    
+    //$_SERVER["DOCUMENT_ROOT"]网站根目录的绝对路径
+
+
 ?>
