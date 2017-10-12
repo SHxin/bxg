23ef924770ca5de6095681cb366b0242e0fe0065
SHxin <984502528@qq.com>
2017-10-12 21:12:40 +0800

    1. 使用了前端模块化技术 require.js 2. 实现了登录状态的判断 3. 实现了退出登录的功能

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,17 +1,38 @@
-$(function () {
-	
-  //侧边栏用户信息的动态切换-------------------------------
-  //不是login页面,要执行的代码
-  if(location.pathname != '/dashboard/login'){
+/**
+ * Created by sh on 2017/10/12.
+ */
+define(['jquery','template','cookie'],function ($,template) {
+  
+  $(function () {
     
-    //1-从cookie中获取userinfo的信息
-    var userinfo = $.cookie('userinfo');
-    userinfo = JSON.parse(userinfo);
+    //不是login页面,要执行的代码
+    if(location.pathname != '/dashboard/login'){
+      
+      //查看用户是否登录
+      if(!$.cookie('PHPSESSID')){
+        location.href = '/dashboard/login';
+      }
+      
+      //侧边栏用户信息的动态切换
+      //1-从cookie中获取userinfo的信息
+      var userinfo = $.cookie('userinfo');
+      userinfo = JSON.parse(userinfo);
+      
+      //2-使用模板引擎将获取到的信息展示到侧边栏
+      var str = template('profile-tpl',userinfo);
+      $('#user-info').html(str);
+      
+    }
     
-    //2-使用模板引擎将获取到的信息展示到侧边栏
-    var str = template('profile-tpl',userinfo);
-    $('#user-info').html(str);
+    //退出按钮的点击事件
+    $('#logout').click(function () {
+      $.post('/api/logout',function (data) {
+        if(data.code == 200){
+          location.href = '/dashboard/login';
+        }
+      });
+    });
     
-  }
+  });
   
 });
