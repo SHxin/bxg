61a1abd6a09a3fa63465666abc2aa07e4d53096d
SHxin <984502528@qq.com>
2017-10-17 11:23:22 +0800

    课程列表完成

--- views/static/js/course/list.js
+++ views/static/js/course/list.js
@@ -1,13 +1,15 @@
 /**
  * Created by sh on 2017/10/16.
  */
-define(['jquery'],function ($) {
+define(['jquery','template'],function ($,template) {
   
   $(function () {
     
     //发送ajax请求
     $.get('/api/course',function (data) {
       console.log(data);
+      var str = template('course-tpl',data);
+      $('.courses').html(str);
     });
   });
 });
